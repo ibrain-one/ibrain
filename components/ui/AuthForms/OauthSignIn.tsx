cbe1a4259689b643dfac3c6b1680ddaf6d492bd3
Martin Ouimet <mouimet@infinisoft.world>
2024-04-06 04:43:50 -0400

    auith oprvider

--- components/ui/AuthForms/OauthSignIn.tsx
+++ components/ui/AuthForms/OauthSignIn.tsx
@@ -3,7 +3,7 @@
 import Button from '@/components/ui/Button';
 import { signInWithOAuth } from '@/utils/auth-helpers/client';
 import { type Provider } from '@supabase/supabase-js';
-import { Github } from 'lucide-react';
+import { Github, Chrome } from 'lucide-react';
 import { useState } from 'react';
 
 type OAuthProviders = {
@@ -18,7 +18,12 @@ export default function OauthSignIn() {
       name: 'github',
       displayName: 'GitHub',
       icon: <Github className="h-5 w-5" />
-    }
+    },
+    {
+      name: 'google',
+      displayName: 'Google',
+      icon: <Chrome className="h-5 w-5" />
+    },
     /* Add desired OAuth providers here */
   ];
   const [isSubmitting, setIsSubmitting] = useState(false);
